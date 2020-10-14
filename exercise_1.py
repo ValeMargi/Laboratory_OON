dc9b815481cd7c8211cbb2976e241f469ae0e175
ValeMargi <s267543@studenti.polito.it>
2020-10-14 18:56:38 +0200

    Modifica ex1 e aggiunta file comandiPython

--- exercise_1.py
+++ exercise_1.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 def my_sum(a, b):
     return a + b
 
@@ -5,5 +8,22 @@ def my_sum(a, b):
 def my_division(a, b):
     return a / b
 
+
 def my_power(base, exponent):
     return base ** exponent
+
+
+def my_sin(n_points, arr):
+    x = np.array([])  # empty array obj
+    y = np.array([i ** 2 for i in range(n_points)])
+    x1 = np.ones(y.shape[0])  # shapes: as output dimension of array
+
+    plt.figure()
+    plt.plot(arr, x1, 'or', label='example')
+    plt.grid()
+    plt.legend()
+    plt.xlabel('xlab')
+    plt.ylabel('ylab')
+    plt.show()
+
+    return np.sin(arr)

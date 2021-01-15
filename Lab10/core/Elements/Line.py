Fix updating switching_matrix and gsnr computation for connections - Lab10

Lab10/core/Elements/Line.py
@@ -96,14 +96,14 @@ class Line(object):
         latency = self.length / (c * 2 / 3)
         return latency
 
-    def noise_generation(self, signal_power):
-        noise = self.ase_generation() + self.nli_generation()
+    def noise_generation(self, signal):
+        noise = self.ase_generation() + self.nli_generation(signal)
         return noise
 
     def propagate(self, signal_information):
 
         if type(signal_information) is Lightpath:
-            if signal_information.channel is not None:
+            if signal_information.channel is not None: # ==1
                 self.state[signal_information.channel] = 0  # 'occupied'
 
         # Update latency
@@ -112,7 +112,8 @@ class Line(object):
 
         # Update noise
         signal_power = signal_information.signal_power
-        noise = self.noise_generation(signal_power)
+        print("path before noise_gen ", signal_information.path)
+        noise = self.noise_generation(signal_information)
         signal_information.add_noise(noise)
 
         # Update gsnr
@@ -130,10 +131,11 @@ class Line(object):
         self.n_amplifiers = (ceil(self.length / 80e3) -1)+2
         return self.n_amplifiers * (h * f * Bn * (10 ** (self.noise_figure / 10)) * ((10 ** (self.gain / 10)) - 1))
 
-    def nli_generation(self ):
+    def nli_generation(self, signal ):
         # print("log", ((np.pi ** 2) * self.beta2 * (Rs ** 2) * (n_channel ** (2 * Rs / df))))
-        nli = Pch_base ** 3 * self.eta_nli_generation() * self.n_span * Bn
-        print("N_SPAN: ", self.n_span, "Bn: ", Bn, "Pch_base: ", Pch_base)
+        nli = signal.signal_power ** 3 * self.eta_nli_generation() * self.n_span * Bn
+        print("PATH signal: ", signal.path)
+        print("N_SPAN: ", self.n_span, "Bn: ", Bn, "Pch_base: ", signal.signal_power)
         print("NLI: ", nli, "ETA: ", self.eta_nli_generation(), "ASE: ", self.ase_generation())
         return nli
 
@@ -146,7 +148,7 @@ class Line(object):
                * (self.alfa / self.beta2 * ((self.gamma ** 2) * (self.Leff ** 2) / (Rs ** 3)))
 
     def gsnr_computation(self, signal):
-        return signal.signal_power / signal.noise_power
+        return signal.signal_power / self.noise_generation(signal)
 
     def isnr_computation(self, signal):
         return 1 / self.gsnr_computation(signal)

Lab10/core/Elements/Network.py
@@ -6,7 +6,7 @@ import json
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import erfcinv as erfcinv
-
+import copy
 n_channel = 10
 ber_t = 1e-3
 Rs = 32e9
@@ -121,7 +121,7 @@ class Network(object):
 
         for node_label in nodes_dict:
             node = nodes_dict[node_label]
-            node.switching_matrix = self.switching_matrix[node_label]
+            node.switching_matrix = copy.deepcopy(self.switching_matrix[node_label])
             # print("Node: ", node_label)
             # print("Switching matrix in node: ", node.switching_matrix)
             for connected_node in node.connected_nodes:
@@ -174,6 +174,7 @@ class Network(object):
                 best_path_array, best_path, channel = self.find_best_latency(connection.input, connection.output)
 
             if best_path is not None:
+                # path string without "->"
                 path_label = ''
                 for index in range(0, len(best_path), 3):
                     path_label += best_path[index]
@@ -187,7 +188,7 @@ class Network(object):
                 else:
                     print("PATH: ", best_path)
                     self.propagate(lightpath)
-                    connection.snr = self.snr_dB(lightpath.signal_power, lightpath.noise_power)
+                    connection.snr = self.snr_dB(lightpath)
                     connection.latency = lightpath.latency
                     connection.bit_rate = bit_rate
                     #print("best path: ", best_path)
@@ -198,8 +199,8 @@ class Network(object):
         # Restore network
         self.restore_network()
 
-    def snr_dB(self, signal_power, noise_power):
-        return 10 * np.log10(signal_power / noise_power)
+    def snr_dB(self, lightpath):
+        return (10 * np.log10(1/lightpath.isnr))
 
     def update_routing_space(self, best_path):
         if best_path is not None:  # routing space not empty
@@ -239,7 +240,7 @@ class Network(object):
         lines_dict = self.lines
         for node_label in nodes_dict:
             node = nodes_dict[node_label]
-            node.switching_matrix = self.switching_matrix[node_label]
+            node.switching_matrix = copy.deepcopy(self.switching_matrix[node_label])
 
         for line_label in lines_dict:
             line = lines_dict[line_label]
@@ -254,6 +255,7 @@ class Network(object):
             path_label += node + '->'
         gsnr_dB = self.weighted_path[self.weighted_path['path'] == path_label[:-2]]['snr'].values[0] #dB
         gsnr = 10**(gsnr_dB/10)
+        print("GSNR linear: ", gsnr, " GSNR dB: " ,gsnr_dB)
         if strategy == 'fixed_rate':
             if gsnr >= 2 * ((erfcinv(2 * ber_t)) ** 2) * lightpath.symbol_rate / Bn:
                 return 100e9

Lab10/tasks/main.py
@@ -32,14 +32,13 @@ if __name__ == '__main__':
                         path_string += node + '->'
                     paths.append(path_string[:-2])
 
+                    print("Path main: ", path)
                     signal_information = SignalInformation(1e-3, path)
                     signal_information = network_fixed_rate.propagate(signal_information)
                     latencies.append(signal_information.latency)
                     noises.append(signal_information.noise_power)
                     snrs.append(10 * np.log10(1/signal_information.isnr))
                     #snrs.append(10 * np.log10(signal_information.signal_power / signal_information.noise_power))
-
-
             df['path'] = paths
             df['latency'] = latencies
             df['noise'] = noises
@@ -201,7 +200,8 @@ if __name__ == '__main__':
     plt.show()
     print("Average bit rate for fixed rate", st.mean(bit_rate_connections))
     print("Total capacity", sum(bit_rate_connections))
-    '''
+
+
     # Shannon
     network_shannon = Network('../resources/nodes_full_shannon.json')
     network_shannon.connect()
@@ -256,4 +256,3 @@ if __name__ == '__main__':
     print("Average bit rate for flex rate", st.mean(bit_rate_connections))
     print("Total capacity", sum(bit_rate_connections))
 
-    '''